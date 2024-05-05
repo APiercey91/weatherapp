9a552dabe18ae352db566883d1326f41da3a464c
APiercey91 <162923218+alexpiercey@users.noreply.github.com>
2024-05-05 13:32:57 -0300

    nasa image of the day

--- scripts/script.js
+++ scripts/script.js
@@ -168,7 +168,42 @@ document.addEventListener('DOMContentLoaded', function () {
        }
    });
 
+
+
+//NASA astronmy image of the day 
+const nasaKey = `4MJHEjfojKO4eJsoLUNCY6IcsMvDuTve718nTmhY`;
+const nasaURL = `https://api.nasa.gov/planetary/apod?api_key=${nasaKey}`;
+
+fetch(nasaURL)
+.then(response => response.json())
+.then(data => {
+
+    console.log(data);
+ let image = data.url;
+let text = data.explanation;
+document.body.style.backgroundImage = `url${image}`;
+
+
+
+
+
+ let aotd = document.getElementById('aotd');
+
+aotd.style.backgroundImage = `url('${image}')`;
+aotd.style.backgroundSize = `cover`;
+aotd.style.backgroundRepeat = 'no-repeat';
+aotd.style.backgroundPosition = 'center';
+
+ aotd.innerHTML = `
+ <p class="font-bold self-start">NASA's AOTD</p>
+ <p class="hidden lg:block lg:text-xl">${text}</p>
+ `;
+
+})
+
+
 });
 
 //TODO: add NASA astronomy image of the day
 //https://api.nasa.gov/
+//API key: 4MJHEjfojKO4eJsoLUNCY6IcsMvDuTve718nTmhY
